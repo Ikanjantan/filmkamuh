f4650a9b1d264df9eab32d44a6e7c2c8b8daf294
Raka Indra <rakaindra210@gmail.com>
2024-04-02 08:14:11 -0700

    first commit

---
+++ src/componen/Anime.js
@@ -0,0 +1,103 @@
+import { Card, Col, Container, Row, Image } from "react-bootstrap"
+
+const Anime = () => {
+    return (
+        <div>
+            <br></br>
+            <Container>
+            <br></br>
+            <h1 className="text-white">ANIME </h1>
+                <Row>
+                    <Col md={4} className="movieWrappper" id="anime">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrappper">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrappper">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrappper">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrappper">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="movieWrappper">
+                        <Card className="bg-dark movieImage">
+                            <Image src={aot} alt="Tatakae" className="imagaes"/>
+                            <div className="bg-dark">
+                                <div className="p-2 m-1 text-white">
+                                <Card.Title className="text-center">Shingeki No Kyojin</Card.Title>
+                                <Card.Text className="text-left">
+                                    Menceritakan pemuda tenpramental yang sangat membenci titan, tetapi dia malah jadi titan
+                                </Card.Text>
+                                <Card.Text className="text-left">Last updated 3 mins ago</Card.Text>
+                                </div>
+                            </div>
+                        </Card>
+                    </Col>
+
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default Anime
